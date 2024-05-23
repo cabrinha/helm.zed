913b3109e8d8769bd6b301bd38c48ec979504164
cabrinha <yuppie@hey.com>
2024-05-23 12:38:42 -0700

    Adding helm-ls

---
+++ src/helm.rs
@@ -0,0 +1,27 @@
+use zed_extension_api::{self as zed, Result};
+
+struct HelmExtension;
+
+impl zed::Extension for HelmExtension {
+    fn new() -> Self {
+        Self
+    }
+
+    fn language_server_command(
+        &mut self,
+        _config: zed::LanguageServerConfig,
+        worktree: &zed::Worktree,
+    ) -> Result<zed::Command> {
+        let path = worktree
+            .which("helm_ls")
+            .ok_or_else(|| "Helm is not installed".to_string())?;
+
+        Ok(zed::Command {
+            command: path,
+            args: vec!["serve".to_string()],
+            env: Default::default(),
+        })
+    }
+}
+
+zed::register_extension!(HelmExtension);
