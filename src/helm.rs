4e8b075c1f59a73d8d2d69d26da115400c1efb35
Martin Lindberg <203184076+eufriction@users.noreply.github.com>
2025-06-25 22:01:58 +0200

    feat: support configuration of helm_ls via lsp settings

--- src/helm.rs
+++ src/helm.rs
@@ -1,6 +1,8 @@
-use zed_extension_api::{self as zed, Result};
+use zed_extension_api::{self as zed, serde_json, settings::LspSettings, LanguageServerId, Result};
 
 struct HelmExtension;
+
+const HELM_LS: &str = "helm_ls";
 
 impl zed::Extension for HelmExtension {
     fn new() -> Self {
@@ -9,11 +11,11 @@ impl zed::Extension for HelmExtension {
 
     fn language_server_command(
         &mut self,
-        _config: zed::LanguageServerConfig,
+        _language_server_id: &LanguageServerId,
         worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
         let path = worktree
-            .which("helm_ls")
+            .which(HELM_LS)
             .ok_or_else(|| "The LSP for helm 'helm-ls' is not installed".to_string())?;
 
         Ok(zed::Command {
@@ -21,6 +23,18 @@ impl zed::Extension for HelmExtension {
             args: vec!["serve".to_string()],
             env: Default::default(),
         })
+    }
+
+    fn language_server_workspace_configuration(
+        &mut self,
+        _language_server_id: &zed::LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<Option<serde_json::Value>> {
+        let settings = LspSettings::for_worktree(HELM_LS, worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.settings.clone())
+            .unwrap_or_default();
+        Ok(Some(settings))
     }
 }
 
